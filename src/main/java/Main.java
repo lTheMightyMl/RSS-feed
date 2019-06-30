e48cca1eae749b358cb0f3e4efd9bc90b42027d7
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-06-30 17:35:03 +0430

    Initialise project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,38 @@
-import com.rometools.rome.feed.synd.SyndFeed;
-import com.rometools.rome.io.FeedException;
-import com.rometools.rome.io.SyndFeedInput;
-import com.rometools.rome.io.XmlReader;
-
-import java.io.IOException;
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) throws IOException, FeedException {
-        URL feedSource = new URL("https://www.isna.ir/rss");
-        SyndFeedInput input = new SyndFeedInput();
-        SyndFeed feed = input.build(new XmlReader(feedSource));
-        feed.getEntries().forEach(System.out::println);
-        try {
-            Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/smsk", "smsk", "");
-            System.out.println("Opened database successfully");
-            Statement stmt = c.createStatement();
-            String sql = "CREATE TABLE COMPANY " +
-                    "(ID INT PRIMARY KEY     NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " AGE            INT     NOT NULL, " +
-                    " ADDRESS        CHAR(50), " +
-                    " SALARY         REAL)";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-        System.out.println("Table created successfully");
+    private static final String RSS_FEED = "jdbc:postgresql://localhost:5432/rss_feed";
+    private static final String USER = "smsk";
+    private static final String PASSWORD = "";
+    private static final String NEWS_AGENCIES = "NEWS_AGENCIES";
+
+    public static void main(String[] args) throws SQLException {
+        init();
     }
-}
+
+    private static void init() throws SQLException {
+        Connection connection = DriverManager.getConnection(RSS_FEED, USER, PASSWORD);
+        connection.setAutoCommit(false);
+        Statement statement = connection.createStatement();
+        try (ResultSet resultSet = statement.executeQuery("SELECT * FROM " + NEWS_AGENCIES + ";")) {
+            System.out.println(resultSet.getFetchSize());
+            while (resultSet.next())
+                System.out.println(resultSet.getString("name"));
+        } catch (SQLException e) {
+            final HashMap<String, String> newsAgencies = new HashMap<>();
+            initDefaultValues(newsAgencies);
+        }
+    }
+        private static void initDefaultValues (HashMap < String, String > newsAgencies){
+            newsAgencies.put("Tasnim News", "https://www.tasnimnews.com/fa/rss/feed/0/7/0/آخرین-اخبار-اخبار-روز");
+
+            newsAgencies.put("ISNA", "https://www.isna.ir/rss");
+
+            newsAgencies.put("IRNA", "https://www.irna.ir/rss");
+
+            newsAgencies.put("SNN", "https://snn.ir/fa/rss/allnews");
+
+            newsAgencies.put("Mehr News", "feed:https://www.mehrnews.com/rss");
+        }
+    }
