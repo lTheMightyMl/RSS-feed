299c94df86c22e2d7d199b798331296e97cefc27
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-07-01 16:48:56 +0430

    Add properties

--- src/main/java/in/nimbo/App.java
+++ src/main/java/in/nimbo/App.java
@@ -1,45 +1,72 @@
 package in.nimbo;
 
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.sql.*;
-import java.util.HashMap;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Properties;
 
 public class App {
-    private static final String RSS_FEED = "jdbc:postgresql://localhost:5432/rss_feed";
-    private static final String USER = "smsk";
-    private static final String PASSWORD = "";
+    private static final String URL;
+    private static final String USER;
+    private static final String PASSWORD;
+    private static final String TABLE;
     private static final String NEWS_AGENCIES = "NEWS_AGENCIES";
 
-    public static void main(String[] args) throws SQLException {
-        init();
-    }
-
-    private static void init() throws SQLException {
-        Connection connection = DriverManager.getConnection(RSS_FEED, USER, PASSWORD);
-        connection.setAutoCommit(false);
-        Statement statement = connection.createStatement();
-        try (ResultSet resultSet = statement.executeQuery("SELECT EXISTS \n" +
-                "(\n" +
-                "\tSELECT 1\n" +
-                "\tFROM information_schema.tables \n" +
-                "\tWHERE table_schema = 'public'\n" +
-                "\tAND table_name = 'NEWS_AGENCIE'\n" +
-                ");")) {
-        } catch (SQLException e) {
-            final HashMap<String, String> newsAgencies = new HashMap<>();
-            initDefaultValues(newsAgencies);
-            System.err.println("here");
+    static {
+        Properties databaseProperties = new Properties();
+        try {
+            databaseProperties.load(new FileInputStream(Thread.currentThread().getContextClassLoader().getResource(
+                    "database.properties").getPath()));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        URL = databaseProperties.getProperty("url");
+        USER = databaseProperties.getProperty("user");
+        PASSWORD = databaseProperties.getProperty("password");
+        TABLE = databaseProperties.getProperty("table");
     }
 
-    private static void initDefaultValues(HashMap<String, String> newsAgencies) {
-        newsAgencies.put("Tasnim News", "https://www.tasnimnews.com/fa/rss/feed/0/7/0/آخرین-اخبار-اخبار-روز");
-
-        newsAgencies.put("ISNA", "https://www.isna.ir/rss");
-
-        newsAgencies.put("IRNA", "https://www.irna.ir/rss");
-
-        newsAgencies.put("SNN", "https://snn.ir/fa/rss/allnews");
-
-        newsAgencies.put("Mehr News", "feed:https://www.mehrnews.com/rss");
+    public static void main(String[] args) throws IOException, FeedException, SQLException {
+        Properties newsAgencies = new Properties();
+        newsAgencies.load(new FileInputStream(Thread.currentThread().getContextClassLoader().getResource("news" +
+                "Agencies.properties").getPath()));
+        Enumeration<?> propertyNames = newsAgencies.propertyNames();
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        while (propertyNames.hasMoreElements()) {
+            String agencyName = propertyNames.nextElement().toString();
+            URL feedSource = new URL(newsAgencies.getProperty(agencyName));
+            SyndFeedInput input = new SyndFeedInput();
+            SyndFeed feed = input.build(new XmlReader(feedSource));
+            System.out.println(agencyName);
+            feed.getEntries().forEach(entry -> {
+                String title = entry.getTitle();
+                Date publishedDate = entry.getPublishedDate();
+                String description = entry.getDescription().getValue();
+                String author = entry.getAuthor();
+                System.out.println(title);
+                System.out.println(publishedDate);
+                System.out.println(description);
+                System.out.println(author);
+                try {
+                    Statement statement = connection.createStatement();
+                    statement.closeOnCompletion();
+                    statement.executeUpdate("INSERT INTO " + TABLE + " VALUES ('" + title + "', '" + publishedDate + "', '" + description + "', '" + author + "');");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM " + TABLE + ";");
+        while (resultSet.next()) {
+            System.err.println(resultSet.getString("title"));
+        }
     }
 }
