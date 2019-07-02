2df1cc65b81c24d5e3e0779c3bf3ea910e3af815
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-07-02 17:07:07 +0430

    Add search by title and date

--- src/main/java/in/nimbo/database/Table.java
+++ src/main/java/in/nimbo/database/Table.java
@@ -15,7 +15,6 @@ public class Table {
     private static final String URL;
     private static final String USER;
     private static final String PASSWORD;
-    private String name;
 
     static {
         Properties databaseProperties = new Properties();
@@ -24,29 +23,65 @@ public class Table {
                     getContextClassLoader().getResource("database.properties")).getPath()));
         } catch (IOException e) {
             LOGGER.error("", e);
+            System.exit(0);
         }
         URL = databaseProperties.getProperty("url");
         USER = databaseProperties.getProperty("user");
         PASSWORD = databaseProperties.getProperty("password");
     }
 
-    public Table(String name) throws SQLException {
+    private final PreparedStatement searchTitle;
+    private final PreparedStatement insert;
+    private final PreparedStatement searchTitleInDate;
+    private final PreparedStatement searchDescriptionInDate;
+
+    public Table(final String name) throws SQLException {
+        Connection insertConnection = DriverManager.getConnection(URL, USER, PASSWORD);
+        Connection searchTitleConnection = DriverManager.getConnection(URL, USER, PASSWORD);
+        Connection searchTitleInDateConnection = DriverManager.getConnection(URL, USER, PASSWORD);
+        Connection searchDescriptionInDateConnection = DriverManager.getConnection(URL, USER, PASSWORD);
         try (final Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-             final PreparedStatement preparedStatement = connection.prepareStatement(String.format("DO $$ BEGIN IF " +
-                     "NOT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name" +
-                     " = '%s') THEN CREATE TABLE %s(agency text, title text, published_date timestamp without time " +
-                     "zone, description text, author text); END IF; END $$;", name, name))) {
-            preparedStatement.executeUpdate();
+             final Statement statement = connection.createStatement()) {
+            statement.executeUpdate(String.format("DO $$ BEGIN IF NOT EXISTS(SELECT 1 FROM information_schema.tables " +
+                    "WHERE table_schema = 'public' AND table_name = '%s') THEN CREATE TABLE %s(agency text, " +
+                    "title text, published_date timestamp without time zone, description text, author text); " +
+                    "END IF; END $$;", name, name));
+            insert = insertConnection.prepareStatement(String.format("INSERT INTO %s VALUES (?, ?, ?, ?, ?)", name));
+            searchTitle = searchTitleConnection.prepareStatement(String.format("SELECT * FROM %s WHERE title ~ ?;", name
+            ));
+            searchTitleInDate = searchTitleInDateConnection.prepareStatement(String.format("SELECT * FROM %s WHERE " +
+                    "title ~ ? AND published_date >= ? AND published_date <= ?;", name));
+            searchDescriptionInDate = searchDescriptionInDateConnection.prepareStatement(String.format("SELECT * FROM %s WHERE " +
+                    "description ~ ? AND published_date >= ? AND published_date <= ?;", name));
         }
-        this.name = name;
     }
 
-    public void insert(String agencyName, String title, Date publishedDate, String description, String author) throws SQLException {
-        try (final Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-             final PreparedStatement preparedStatement = connection.prepareStatement(String.format("INSERT INTO %s " +
-                     "VALUES ('%s', '%s', TIMESTAMP (6) WITHOUT TIME ZONE '%s', ' %s', '%s')", name, agencyName, title, new
-                     Timestamp(publishedDate.getTime()), description, author))) {
-            preparedStatement.executeUpdate();
-        }
+    public void insert(final String agencyName, final String title, final Date publishedDate, final String description,
+                       final String author) throws SQLException {
+        insert.setString(1, agencyName);
+        insert.setString(2, title);
+        insert.setTimestamp(3, new Timestamp(publishedDate.getTime()));
+        insert.setString(4, description);
+        insert.setString(5, author);
+        insert.executeUpdate();
+    }
+
+    public ResultSet searchTitle(final String title) throws SQLException {
+        searchTitle.setString(1, title);
+        return searchTitle.executeQuery();
+    }
+
+    public ResultSet searchTitleInDate(final String title, final Date from, final Date to) throws SQLException {
+        searchTitleInDate.setString(1, title);
+        searchTitleInDate.setTimestamp(2, new Timestamp(from.getTime()));
+        searchTitleInDate.setTimestamp(3, new Timestamp(to.getTime()));
+        return searchTitleInDate.executeQuery();
+    }
+
+    public ResultSet searchDescriptionInDate(String description, Date from, Date to) throws SQLException {
+        searchDescriptionInDate.setString(1, description);
+        searchDescriptionInDate.setTimestamp(2, new Timestamp(from.getTime()));
+        searchDescriptionInDate.setTimestamp(3, new Timestamp(to.getTime()));
+        return searchDescriptionInDate.executeQuery();
     }
 }
