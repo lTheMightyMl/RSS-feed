176fc60a7cef8b89430441100b212cf0169e396a
ali aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-07-02 14:51:01 +0430

    fix gitignore, delete unnececery files and configuring base things

--- src/main/java/in/nimbo/App.java
+++ src/main/java/in/nimbo/App.java
@@ -1,72 +1,69 @@
 package in.nimbo;
 
+import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
+import in.nimbo.database.Table;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.sql.*;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.Enumeration;
+import java.util.Objects;
 import java.util.Properties;
 
 public class App {
-    private static final String URL;
-    private static final String USER;
-    private static final String PASSWORD;
-    private static final String TABLE;
-    private static final String NEWS_AGENCIES = "NEWS_AGENCIES";
-
-    static {
-        Properties databaseProperties = new Properties();
-        try {
-            databaseProperties.load(new FileInputStream(Thread.currentThread().getContextClassLoader().getResource(
-                    "database.properties").getPath()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        URL = databaseProperties.getProperty("url");
-        USER = databaseProperties.getProperty("user");
-        PASSWORD = databaseProperties.getProperty("password");
-        TABLE = databaseProperties.getProperty("table");
-    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(Table.class);
 
     public static void main(String[] args) throws IOException, FeedException, SQLException {
+        writeToDB();
+    }
+
+    private static void writeToDB() throws IOException, FeedException, SQLException {
+        final Table rssFeeds = new Table("rss_feeds");
+        Properties newsAgencies = loadAgencies();
+        Enumeration<?> agencyNames = newsAgencies.propertyNames();
+        while (agencyNames.hasMoreElements()) {
+            Object agency = agencyNames.nextElement();
+            processAgency(rssFeeds, agency.toString(), newsAgencies.getProperty(agency.toString()));
+        }
+    }
+
+    private static void processAgency(Table table, String agencyName, String agencyURL) throws FeedException,
+            IOException {
+        LOGGER.info(agencyName);
+        SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(agencyURL)));
+        feed.getEntries().forEach(entry -> processEntry(table, agencyName, entry));
+    }
+
+    private static void processEntry(Table table, String agencyName, SyndEntry entry) {
+        String title = entry.getTitle();
+        Date publishedDate = entry.getPublishedDate();
+        String description = entry.getDescription().getValue();
+        String author = entry.getAuthor();
+        LOGGER.info(title);
+        final String publishedDateString = publishedDate.toString();
+        LOGGER.info(publishedDateString);
+        LOGGER.info(description);
+        LOGGER.info(author);
+        LOGGER.info("");
+        try {
+            table.insert(agencyName, title, publishedDate, description, author);
+        } catch (SQLException e) {
+            LOGGER.error("", e);
+        }
+    }
+
+    private static Properties loadAgencies() throws IOException {
         Properties newsAgencies = new Properties();
-        newsAgencies.load(new FileInputStream(Thread.currentThread().getContextClassLoader().getResource("news" +
-                "Agencies.properties").getPath()));
-        Enumeration<?> propertyNames = newsAgencies.propertyNames();
-        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        while (propertyNames.hasMoreElements()) {
-            String agencyName = propertyNames.nextElement().toString();
-            URL feedSource = new URL(newsAgencies.getProperty(agencyName));
-            SyndFeedInput input = new SyndFeedInput();
-            SyndFeed feed = input.build(new XmlReader(feedSource));
-            System.out.println(agencyName);
-            feed.getEntries().forEach(entry -> {
-                String title = entry.getTitle();
-                Date publishedDate = entry.getPublishedDate();
-                String description = entry.getDescription().getValue();
-                String author = entry.getAuthor();
-                System.out.println(title);
-                System.out.println(publishedDate);
-                System.out.println(description);
-                System.out.println(author);
-                try {
-                    Statement statement = connection.createStatement();
-                    statement.closeOnCompletion();
-                    statement.executeUpdate("INSERT INTO " + TABLE + " VALUES ('" + title + "', '" + publishedDate + "', '" + description + "', '" + author + "');");
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-        ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM " + TABLE + ";");
-        while (resultSet.next()) {
-            System.err.println(resultSet.getString("title"));
-        }
+        newsAgencies.load(new FileInputStream(Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("news" +
+                "Agencies.properties")).getPath()));
+        return newsAgencies;
     }
 }
