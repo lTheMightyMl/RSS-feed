495f7059d60a046873bdfc48c090f2aa2fafff8e
ali aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-07-04 20:55:49 +0430

    add base files

---
+++ src/test/java/in/nimbo/ExternalDataTest.java
@@ -0,0 +1,35 @@
+package in.nimbo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExternalDataTest {
+
+    /**
+     * Only in case of catching exception this method is wrong
+     */
+    @Test
+    public void loadProperties() {
+    }
+
+    /**
+     * it must give a exception to pass the test
+     */
+    @Test
+    public void badLoadProperties() {
+
+    }
+
+    @Test
+    public void getPropertyValue() {
+    }
+
+    @Test
+    public void addProperty() {
+    }
+
+    @Test
+    public void getAllAgencies() {
+    }
+}
