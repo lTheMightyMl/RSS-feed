a1700bd0fcb71c5e76105db91b0e964487a80632
ali aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-07-08 21:02:31 +0430

    add class for test Table class

---
+++ src/test/java/in/nimbo/database/TableTest.java
@@ -0,0 +1,40 @@
+package in.nimbo.database;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TableTest {
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void searchTitle() {
+    }
+
+    @Test
+    public void searchTitleInDate() {
+    }
+
+    @Test
+    public void searchDescriptionInDate() {
+    }
+
+    @Test
+    public void searchOnTitleInSpecificSite() {
+    }
+
+    @Test
+    public void searchOnContentInSpecificSite() {
+    }
+
+    @Test
+    public void searchOnContent() {
+    }
+
+    @Test
+    public void close() {
+    }
+}
