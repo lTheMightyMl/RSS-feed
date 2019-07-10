9f7b0a9506946eb7eceab9104074f3041a34c230
ali aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-07-10 14:55:29 +0430

    change table queries to work for h2 as well

--- src/main/java/in/nimbo/database/Table.java
+++ src/main/java/in/nimbo/database/Table.java
@@ -1,5 +1,6 @@
 package in.nimbo.database;
 
+import in.nimbo.App;
 import in.nimbo.ExternalData;
 
 import java.sql.*;
@@ -7,12 +8,12 @@ import java.util.Date;
 
 public class Table {
     private static final String OFFSET_ROWS_FETCH_NEXT_ROWS_ONLY = "OFFSET ? ROWS FETCH NEXT ? ROWS ONLY;";
-    private final PreparedStatement searchTitle;
-    private final PreparedStatement searchTitleInDate;
-    private final PreparedStatement searchDescriptionInDate;
-    private final PreparedStatement searchOnTitleInSpecificSite;
-    private final PreparedStatement searchOnContentInSpecificSite;
-    private final PreparedStatement searchOnContent;
+    private PreparedStatement searchTitle;
+    private PreparedStatement searchTitleInDate;
+    private PreparedStatement searchDescriptionInDate;
+    private PreparedStatement searchOnTitleInSpecificSite;
+    private PreparedStatement searchOnContentInSpecificSite;
+    private PreparedStatement searchOnContent;
     private String url;
     private String user;
     private String password;
@@ -41,23 +42,8 @@ public class Table {
              PreparedStatement prestatement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS " + name +
                      "(agency text, title text, published_date timestamp (6) without time zone," +
                      " description text, author text);")) {
-            prestatement.executeUpdate();
-            searchTitle = searchTitleConnection.prepareStatement("SELECT * FROM ? WHERE title ~ ? OFFSET ? ROWS " +
-                    "FETCH NEXT ? ROWS ONLY;");
-            searchTitleInDate = searchTitleInDateConnection.prepareStatement("SELECT * FROM ? WHERE " +
-                    "title ~ ? AND published_date >= ? AND published_date <= ? " +
-                    OFFSET_ROWS_FETCH_NEXT_ROWS_ONLY);
-            searchDescriptionInDate = searchDescriptionInDateConnection.prepareStatement("SELECT * " +
-                    "FROM ? WHERE description ~ ? AND published_date >= ? AND published_date <= ? " +
-                    OFFSET_ROWS_FETCH_NEXT_ROWS_ONLY);
-            searchOnTitleInSpecificSite = searchOnTitleInSpecificSiteConnection.prepareStatement(
-                    "SELECT * FROM ? WHERE agency = ? AND title ~ ? " +
-                            OFFSET_ROWS_FETCH_NEXT_ROWS_ONLY);
-            searchOnContentInSpecificSite = searchOnContentInSpecificSiteConnection.prepareStatement(
-                    "SELECT * FROM ? WHERE agency = ? AND description ~ ? " +
-                            OFFSET_ROWS_FETCH_NEXT_ROWS_ONLY);
-            searchOnContent = searchOnContentConnection.prepareStatement("SELECT * FROM ? WHERE " +
-                    "description ~ ? OFFSET ? ROWS FETCH NEXT ? ROWS ONLY;");
+            boolean b = prestatement.execute();
+            System.out.println("table created? : " + b);
         }
         this.name = name;
     }
@@ -75,29 +61,42 @@ public class Table {
         if (author == null || author.isEmpty())
             author = " ";
         try (Connection connection = DriverManager.getConnection(url, user, password);
-             PreparedStatement prepstatement = connection.prepareStatement("DO $$ BEGIN IF NOT EXISTS(SELECT 1 " +
-                     "FROM " + name + " WHERE agency = $one$" + agencyName + "$one$ AND title = $two$" + title +
-                     "$two$) THEN INSERT INTO " + name + "(agency, title, published_date, description, author) " +
-                     "VALUES ($three$" + agencyName + "$three$, $four$" + title + "$four$, TIMESTAMP (6) WITH TIME " +
-                     "ZONE $five$" + new Timestamp(publishedDate.getTime()) + "$five$, $six$" + description +
-                     "$six$, $seven$" + author + "$seven$); END IF; END $$;")) {
-            prepstatement.executeUpdate();
+             PreparedStatement pscheck = connection.prepareStatement("select  * from  " + name + " where " +
+                     "agency = ? and title = ?");
+             PreparedStatement psinsert = connection.prepareStatement("insert into " + name + " values (?, ?, ?, ?, ?)")) {
+            pscheck.setString(1, agencyName);
+            pscheck.setString(2, title);
+            ResultSet resultSet = pscheck.executeQuery();
+            if (App.resultSetSize(resultSet) == 0) {
+                psinsert.setString(1, agencyName);
+                psinsert.setString(2, title);
+                psinsert.setTimestamp(3, new Timestamp(publishedDate.getTime()));
+                psinsert.setString(4, description);
+                psinsert.setString(5, author);
+                psinsert.executeUpdate();
+            }
         }
     }
 
     public ResultSet searchTitle(String title, int offset, int count) throws SQLException {
         if (searchTitleConnection.isClosed())
             searchTitleConnection = DriverManager.getConnection(url, user, password);
+        searchTitle = searchTitleConnection.prepareStatement("SELECT * FROM ? WHERE title ~ ? OFFSET ? ROWS " +
+                "FETCH NEXT ? ROWS ONLY;");
         searchTitle.setString(1, name);
         searchTitle.setString(2, title);
         searchTitle.setInt(3, offset);
         searchTitle.setInt(4, count);
+        System.out.println(searchTitle);
         return searchTitle.executeQuery();
     }
 
     public ResultSet searchTitleInDate(final String title, final Date from, final Date to, int offset, int count) throws SQLException {
         if (searchTitleInDateConnection.isClosed())
             searchTitleInDateConnection = DriverManager.getConnection(url, user, password);
+        searchTitleInDate = searchTitleInDateConnection.prepareStatement("SELECT * FROM ? WHERE " +
+                "title ~ ? AND published_date >= ? AND published_date <= ? " +
+                OFFSET_ROWS_FETCH_NEXT_ROWS_ONLY);
         searchTitleInDate.setString(1, name);
         searchTitleInDate.setString(2, title);
         searchTitleInDate.setTimestamp(3, new Timestamp(from.getTime()));
@@ -110,6 +109,9 @@ public class Table {
     public ResultSet searchDescriptionInDate(String description, Date from, Date to, int offset, int count) throws SQLException {
         if (searchTitleInDateConnection.isClosed())
             searchTitleInDateConnection = DriverManager.getConnection(url, user, password);
+        searchDescriptionInDate = searchDescriptionInDateConnection.prepareStatement("SELECT * " +
+                "FROM ? WHERE description ~ ? AND published_date >= ? AND published_date <= ? " +
+                OFFSET_ROWS_FETCH_NEXT_ROWS_ONLY);
         searchDescriptionInDate.setString(1, name);
         searchDescriptionInDate.setString(2, description);
         searchDescriptionInDate.setTimestamp(3, new Timestamp(from.getTime()));
@@ -122,6 +124,9 @@ public class Table {
     public ResultSet searchOnTitleInSpecificSite(String agencyName, String title, int offset, int count) throws SQLException {
         if (searchOnTitleInSpecificSiteConnection.isClosed())
             searchOnTitleInSpecificSiteConnection = DriverManager.getConnection(url, user, password);
+        searchOnTitleInSpecificSite = searchOnTitleInSpecificSiteConnection.prepareStatement(
+                "SELECT * FROM ? WHERE agency = ? AND title ~ ? " +
+                        OFFSET_ROWS_FETCH_NEXT_ROWS_ONLY);
         searchOnTitleInSpecificSite.setString(1, name);
         searchOnTitleInSpecificSite.setString(2, agencyName);
         searchOnTitleInSpecificSite.setString(3, title);
@@ -133,6 +138,9 @@ public class Table {
     public ResultSet searchOnContentInSpecificSite(String agencyName, String content, int offset, int count) throws SQLException {
         if (searchOnContentInSpecificSiteConnection.isClosed())
             searchOnContentInSpecificSiteConnection = DriverManager.getConnection(url, user, password);
+        searchOnContentInSpecificSite = searchOnContentInSpecificSiteConnection.prepareStatement(
+                "SELECT * FROM ? WHERE agency = ? AND description ~ ? " +
+                        OFFSET_ROWS_FETCH_NEXT_ROWS_ONLY);
         searchOnContentInSpecificSite.setString(1, name);
         searchOnContentInSpecificSite.setString(2, agencyName);
         searchOnContentInSpecificSite.setString(3, content);
@@ -144,6 +152,8 @@ public class Table {
     public ResultSet searchOnContent(String content, int offset, int count) throws SQLException {
         if (searchOnContentConnection.isClosed())
             searchOnContentConnection = DriverManager.getConnection(url, user, password);
+        searchOnContent = searchOnContentConnection.prepareStatement("SELECT * FROM ? WHERE " +
+                "description ~ ? OFFSET ? ROWS FETCH NEXT ? ROWS ONLY;");
         searchOnContent.setString(1, name);
         searchOnContent.setString(2, content);
         searchOnContent.setInt(3, offset);
